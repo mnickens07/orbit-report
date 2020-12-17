7c2bfd3a18f812711e98b562a3e68d466d4e0797
mnickens07 <58342630+mnickens07@users.noreply.github.com>
2020-12-17 08:27:14 -0600

    added the ngFor

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -8,7 +8,6 @@ import { Satellite } from '../satellite';
 })
 export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
-
   constructor() { }
 
   ngOnInit() {
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -7,10 +7,11 @@ export class Satellite {
         
         constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
             this.name=name;
-            this.orbitType=orbitType;
             this.type=type;
-            this.operational=operational;
             this.launchDate=launchDate;
+            this.orbitType=orbitType;
+            this.operational=operational;
+            
     }
     
         
